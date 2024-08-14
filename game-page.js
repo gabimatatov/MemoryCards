08b97b15f5f4b1d3e322cd41aa928ce07441694a
gabimatatov <gavriel.matatov@veeva.com>
2024-08-14 21:01:16 +0300

    changed DisplayCard() function

--- game-page.js
+++ game-page.js
@@ -17,6 +17,17 @@ function initializeGame() {
     displayCards();
 }
 
+let seconds = -1;
+function initializeTimer() {
+    seconds++;
+    document.getElementById('timer').textContent = seconds;
+}
+
+// Update the timer every second
+setInterval(initializeTimer, 1000);
+
+
+// Card Game
 function displayCards() {
     const numOfCards = parseInt(getGameParameters('cardsNumberRequested') * 2);
     const gridContainer = document.getElementById('grid-container');
@@ -35,19 +46,8 @@ function displayCards() {
     for (let i = 0; i < numOfCards; i++) {
         const card = document.createElement('div');
         card.className = 'card';
-        card.dataset.value = i % (numOfCards / 2) + 1; // Example value assignment
-        card.innerHTML = `<img src="https://via.placeholder.com/100?text=${card.dataset.value}" alt="Card ${card.dataset.value}">`;
+        // card.dataset.value = i % (numOfCards / 2) + 1; // Example value assignment
+        // card.innerHTML = `<img src="https://via.placeholder.com/100?text=${card.dataset.value}" alt="Card ${card.dataset.value}">`;
         gridContainer.appendChild(card);
     }
 }
-
-
-let seconds = -1;
-function initializeTimer() {
-    seconds++;
-    document.getElementById('timer').textContent = seconds;
-}
-
-// Update the timer every second
-setInterval(initializeTimer, 1000);
-
